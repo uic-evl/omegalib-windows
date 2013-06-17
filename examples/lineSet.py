3f5baa09e0549854f8b77f5a438e34da8323f6ef
Febret <Febret@Narvi.(none)>
2013-06-16 23:07:53 -0500

    initial commit

---
+++ examples/lineSet.py
@@ -0,0 +1,10 @@
+# Basic example showing how to use a line set to connect spheres.
+from math import *
+from euclid import *
+from omega import *
+from cyclops import *
+
+# TODO
+
+scene = getSceneManager()
+
---
+++ python/Lib/xml/parsers/expat.py
@@ -0,0 +1,4 @@
+"""Interface to the Expat non-validating XML parser."""
+__version__ = '$Revision: 2552 $'
+
+from pyexpat import *
