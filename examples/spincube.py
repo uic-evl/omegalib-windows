93f017722ce44ca5d68a189b45facd7f1bde7b46
Alessandro Febretti <febret@gmail.com>
2013-09-18 20:08:31 -0500

    experimental python threading build (4.2.3-x)

--- examples/spincube.py
+++ examples/spincube.py
@@ -8,9 +8,30 @@ box.setPosition(Vector3(0, 2, -3))
 # Apply an emissive textured effect (no lighting)
 box.setEffect("textured -v emissive -d cyclops/test/omega-transparent.png")
 
+test = Text3D.create('fonts/arial.ttf', 1, "TEST")
+test.setFacingCamera(getDefaultCamera())
+test.setColor(Color("red"))
+test.setPosition(Vector3(0,2,-3))
+import time
+def hey():
+	while True:
+		print "hey"
+		time.sleep(1)
+def ooh():
+	while True:
+		print "ooh"
+		time.sleep(0.01)
+
 # Spin the box!
 def onUpdate(frame, t, dt):
 	box.pitch(dt)
 	box.yaw(dt / 3)
+	print "AA"
 setUpdateFunction(onUpdate)
+
+import threading
+t = threading.Thread(target = ooh)
+t.start()
+
+
 
